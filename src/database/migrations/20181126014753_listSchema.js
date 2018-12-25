5c729cf68ad0474d61bf760a4b64a8880762f8b9
letarzan <leandrorevolve@gmail.com>
2018-12-25 02:15:36 -0200

    Ajuste em List - adicionando note

--- src/database/migrations/20181126014753_listSchema.js
+++ src/database/migrations/20181126014753_listSchema.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
           table.integer('qtd')
           table.decimal('amount')
           table.string('color')
+          table.string('note')
           table.timestamps(true, true)
           table.boolean('status').defaultTo(true)
           table.datetime('deactivated_at')
