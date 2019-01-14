8747958424bcdec6ae8891b57e8f565e6ac96e60
Rafael Sanches <fucazu@gmail.com>
2019-01-14 14:11:33 -0200

    Adicionando autenticacao por token

--- src/config/passport/index.js
+++ src/config/passport/index.js
@@ -1,9 +1,12 @@
 const passport = require('passport')
 
 const LocalStrategy = require('passport-local')
+const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt')
 
 const { getUserByUsername } = require('../../services/users')
 const { comparePassword } = require('../../services/password')
+
+const secret = 'aqui digita qualquer coisa... !@#$!#!@#!'
 
 passport.use(new LocalStrategy(async function (username, password, done) {
   try {
@@ -23,3 +26,29 @@ passport.use(new LocalStrategy(async function (username, password, done) {
     return done(error)
   }
 }))
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: secret
+}
+
+passport.use(new JwtStrategy(jwtOptions, async (payload, done) => {
+  try {
+    let user = await // aqui coloca o serviço para lista os dados do usuário
+    console.log('passport.use user = ', user)
+    if (!user) {
+      return done(null, false)
+    }
+    done(null, user)
+  } catch (error) {
+    return done(null, false)
+  }
+}))
+
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})
